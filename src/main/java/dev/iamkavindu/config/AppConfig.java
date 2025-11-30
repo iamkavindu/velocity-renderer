1f3c9acbc6e8289012472ac4d8f47ac93de46629
Kavindu Perera <59706367+kavindu-pere@users.noreply.github.com>
2025-12-01 02:05:12 +0530

    Configure VelocityEngine with custom properties for resource loading and logging

--- src/main/java/dev/iamkavindu/config/AppConfig.java
+++ src/main/java/dev/iamkavindu/config/AppConfig.java
@@ -9,6 +9,14 @@ public class AppConfig {
 
     @Bean
     public VelocityEngine velocityEngine() {
-        return new VelocityEngine();
+        VelocityEngine engine = new VelocityEngine();
+
+        engine.setProperty("resource.loaders", "string");
+        engine.setProperty("resource.loader.string.class", "org.apache.velocity.runtime.resource.loader.StringResourceLoader");
+        engine.setProperty("runtime.log.name", "velocity");
+        engine.setProperty("parser.pool.size", "20");
+
+        engine.init();
+        return engine;
     }
 }
