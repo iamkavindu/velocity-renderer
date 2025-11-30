e28add8968e9faf512b312e09cc47435baea05fa
Kavindu Perera <59706367+kavindu-pere@users.noreply.github.com>
2025-11-30 15:31:01 +0530

    Add configuration for VelocityEngine and update application properties and version

--- src/main/java/dev/iamkavindu/config/AppConfig.java
+++ src/main/java/dev/iamkavindu/config/AppConfig.java
@@ -1,4 +1,14 @@
-package dev.iamkavindu;
+package dev.iamkavindu.config;
 
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    public VelocityEngine velocityEngine() {
+        return new VelocityEngine();
+    }
 }
